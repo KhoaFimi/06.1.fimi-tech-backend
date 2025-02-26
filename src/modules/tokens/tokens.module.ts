e0c4efcd8ad5ec45fb4fb298025de756e309e0e3
Khoa Tran <khoa.trandang30620@gmail.com>
2025-02-26 13:43:10 +0700

    #0 fix:

--- src/modules/tokens/tokens.module.ts
+++ src/modules/tokens/tokens.module.ts
@@ -6,12 +6,11 @@ import { ChangeEmailTokenService } from '@/modules/tokens/services/change-email-
 import { RefreshTokenService } from '@/modules/tokens/services/refresh-token.service'
 import { ResetPasswordTokenService } from '@/modules/tokens/services/reset-password-token.service'
 import { VerificationTokenService } from '@/modules/tokens/services/verification-token.service'
-import { UsersModule } from '@/modules/users/users.module'
 import { UsersService } from '@/modules/users/users.service'
 import { SharedModule } from '@/shared/shared.module'
 
 @Module({
-	imports: [JwtModule.register({}), SharedModule, UsersModule],
+	imports: [JwtModule.register({}), SharedModule],
 	providers: [
 		UsersService,
 		VerificationTokenService,
